355962673add1065f46831a49de6a9a995f1b009
Yegor G <yegorich_555@mail.ru>
2019-02-18 16:46:56 +0300

    Add ref to Atmel-Library

--- ShoesFan_app/ShoesFan_app/main.c
+++ ShoesFan_app/ShoesFan_app/main.c
@@ -5,8 +5,13 @@
  * Author : Yegor
  */ 
 
+#define F_CPU 9600000UL
 #include <avr/io.h>
+#include <extensions.h>
 
+#define IO_OutLed B, 0
+#define IO_InLed B, 2
+#define IO_OutFan B, 4
 
 int main(void)
 {
@@ -14,4 +19,3 @@ int main(void)
     {
     }
 }
-
